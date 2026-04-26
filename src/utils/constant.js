c4b78dc8932da5886713121e5b23eb169641bfae
Aaditya <aadityakumarchoudhary771@gmail.com>
2026-04-26 12:48:49 +0530

    fix: use full backend URL for production

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,6 @@
-export const USER_API_END_POINT = "/api/v1/user"
-export const JOB_API_END_POINT = "/api/v1/job"
-export const APPLICATION_API_END_POINT = "/api/v1/application"
-export const COMPANY_API_END_POINT = "/api/v1/company"
+const BASE_URL = "https://job-portal-1f95.onrender.com"
+
+export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`
+export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`
+export const APPLICATION_API_END_POINT = `${BASE_URL}/api/v1/application`
+export const COMPANY_API_END_POINT = `${BASE_URL}/api/v1/company`
